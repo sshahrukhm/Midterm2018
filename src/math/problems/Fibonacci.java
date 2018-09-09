bf15736a1c22ef3101406c1db3ea5ceb9b962f69
sshahrukhm <sshahrukhm@outlook.com>
2018-09-09 12:45:46 -0400

    fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,10 +2,12 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
-         /*
-          Write 40 Fibonacci numbers with java.
-         */
-
-
+        int a =0;
+        int b = 1;
+        for(int i = 0; i<40; i++){
+            System.out.println(a);
+            a= a+b;
+            b= a-b;
+        }
     }
 }
