ba30007aad0ef750ac292a1ad26de71589c12da3
sshahrukhm <sshahrukhm@outlook.com>
2018-09-09 17:29:23 -0400

    PrimeNumber

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -34,12 +34,10 @@ public class PrimeNumber {
 
     public static boolean isPrime(int n) {
 
-        if
-        (n % 2 == 0) return false;
+        if (n % 2 == 0) return false;
         for (int i = 3; i * i <= n; i += 2) {
 
-            if
-            (n % i == 0) return false;
+            if (n % i == 0) return false;
         }
 
         return true;
