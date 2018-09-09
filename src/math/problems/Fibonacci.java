02defe72599538e63b01c84a4ba62fe5b97c05d7
sshahrukhm <sshahrukhm@outlook.com>
2018-09-09 14:36:38 -0400

    conceptual-questions

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,6 +2,9 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
+         /*
+          Write 40 Fibonacci numbers with java.
+         */
         int a =0;
         int b = 1;
         for(int i = 0; i<40; i++){
